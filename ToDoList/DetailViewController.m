57ac8dc45397d549666eaa88c597b61d46fdf145
CT4TuEI3 <alexstatiev@gmail.com>
2024-05-22 12:18:52 +0300

    Настройка окна создания напоминания

---
+++ ToDoList/DetailViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by CT4TuEI3 on 20.05.2024.
+//
+
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
