0dd5903f7781c59bcd02fbefc13390c26d0c6e43
CT4TuEI3 <alexstatiev@gmail.com>
2024-05-22 12:55:16 +0300

    Программирование UITextField

--- ToDoList/DetailViewController.m
+++ ToDoList/DetailViewController.m
@@ -8,7 +8,7 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 @property (weak, nonatomic) IBOutlet UIButton *buttonAction;
@@ -19,11 +19,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.buttonAction addTarget:self action:@selector(save) forControlEvents:UIControlEventTouchUpInside];
+    [self.buttonAction addTarget:self
+                          action:@selector(save)
+                forControlEvents:UIControlEventTouchUpInside];
+    UITapGestureRecognizer * handleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                 action:@selector(handleEndEditing)];
+    [self.view addGestureRecognizer:handleTap];
 }
 
 - (void)save {
     
+}
+
+- (void)handleEndEditing {
+    [self.view endEditing:YES];    
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([textField isEqual:self.textField]) {
+        [self.textField resignFirstResponder];
+    }
+    return  YES;
 }
 
 @end
